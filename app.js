2e6c40b2470956db021a87d9bb2909f85093ce1f
Richard <richardlwhittington@gmail.com>
2023-04-29 15:37:52 -0500

    set up server

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+//jshint esversion:6
 const express = require("express");
+const bodyParser = require("body-parser");
+
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
+  
+
+
+
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server listening on Port 3000")
+})
