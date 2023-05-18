24b5202738f773cd8fe842bc02c1014d2b7f2498
Richard <richardlwhittington@gmail.com>
2023-05-17 21:52:24 -0500

    add router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const router = express.Router();
 
 
 const app = express();
@@ -12,13 +13,21 @@ app.use(express.static("public"));
     res.sendFile(__dirname + "/index.html");
   });
   
+  
+
+  router.post('/api/register', (req, res) => {
+    // Handle the registration logic here
+    // Access request body data using req.body
+    // Perform necessary validation, data processing, and database operations
+    // Send back the appropriate response using res.json() or res.send()
+  });
+
+  app.use('/', router);
 
 
+  module.exports = router;
 
 
-
-
-
-app.listen(3000, ()=>{
-    console.log("Server listening on Port 3000")
+app.listen(4000, ()=>{
+    console.log("Server listening on Port 4000")
 })
