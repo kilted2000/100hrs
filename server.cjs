1271c26ec02136e7ae8c75f74161268ea534c37c
Richard <richardlwhittington@gmail.com>
2023-06-04 13:02:22 -0500

    add routing folder

--- server.cjs
+++ server.cjs
@@ -1,29 +1,15 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const router = express.Router();
-const https = require("https");
-const fetch = require('node-fetch');
-const cool = require("cool-ascii-faces");
-
 const app = express();
-
-require("@babel/core").transform("code", {
-  presets: ["@babel/preset-env"],
-});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-
-//either use codecamp article or deupdate node-fetch to v2
-
-
-app.use('/', router);
 
 app.listen(3000, () => {
   console.log("Server listening on Port 3000");
