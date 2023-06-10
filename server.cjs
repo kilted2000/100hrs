1b66d79a44a044fbfb9f3b89aa36e7a42b14f2c0
Richard <richardlwhittington@gmail.com>
2023-06-10 17:43:39 -0500

    require bootstrap

--- server.cjs
+++ server.cjs
@@ -1,6 +1,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const bootstrap = require('bootstrap')
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
